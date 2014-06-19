fb04caf5943a0eae02b872682dec92d6748a6319
chrissharkman <ch.sharkman@gmail.com>
2014-06-19 11:58:04 +0200

    Adds User Authentification and Events.php

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -2,24 +2,41 @@
 
 class AuthController extends \BaseController {
 
-	/**
-	 * Login the current user.
-	 *
-	 * @return Response
-	 */
-	public function login()
-	{
-		// there it is
-	}
+    /**
+     * Login the current user. Checks if email and password are set, if they are valid
+     * following User Model, then try to log in.
+     *
+     * @return Jsend success or Jsend fail
+     */
+    public function login() {
+        $credentials = [
+            'email' => Input::get('email'),
+            'password' => Input::get('password')
+        ];
+        if (($credentials['email'] != null && $credentials['password'] != null) &&
+                User::validate(array($credentials['email'], $credentials['password'])) &&
+                Auth::attempt($credentials)) {
+            // get the remember boolean, if set true, so push remember cookie to client
+            $remember = Input::get('remember');
+            if(!empty($remember) && $remember) {
+                Auth::login(Auth::user()->id, true);
+            }
+            return Jsend::success("message to define");
+        }
+        return Jsend::fail("message to define");
+    }
 
-	/**
-	 * Logout the current user.
-	 *
-	 * @return Response
-	 */
-	public function logout()
-	{
-		//
-	}
+    /**
+     * Logout the current user.
+     * @return Jsend success or Jsend fail
+     */
+    public function logout() {
+        try {
+            Auth::logout();
+        } catch (Exception $e) {
+            Jsend::fail("message to define");
+        }
+        return Jsend::success("message to define");
+    }
 
 }
--- app/models/User.php
+++ app/models/User.php
@@ -7,20 +7,83 @@ use Illuminate\Auth\Reminders\RemindableInterface;
 
 class User extends Eloquent implements UserInterface, RemindableInterface {
 
-	use UserTrait, RemindableTrait;
+    use UserTrait,
+        RemindableTrait;
 
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
-	protected $table = 'users';
+    protected static $rules = array(
+        'email' => 'email|max:300|unique:users',
+        'password' => 'min:4|max:2000'
+    );
 
-	/**
-	 * The attributes excluded from the model's JSON form.
-	 *
-	 * @var array
-	 */
-	protected $hidden = array('password', 'remember_token');
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = array('password', 'remember_token');
+
+    /**
+     * Static function to validate User data
+     * @param array('email', 'password') to valid these data
+     * @return boolean true if data is validated, false if not
+     */
+    public static function validate($data = array()) {
+        $v = Validator::make($data, User::$rules);
+        return $v->passes();
+    }
+
+    /**
+     * Get the unique identifier for the user.
+     *
+     * @return mixed
+     */
+    public function getAuthIdentifier() {
+        return $this->getKey();
+    }
+
+    /**
+     * Get the password for the user.
+     *
+     * @return string
+     */
+    public function getAuthPassword() {
+        return $this->password;
+    }
+
+    /**
+     * Get the e-mail address where password reminders are sent.
+     * @return string
+     */
+    public function getReminderEmail() {
+        return $this->email;
+    }
+
+    /**
+     * Get the token value for the "remember me" session.
+     */
+    public function getRememberToken() {
+        // not implemented yet
+    }
+
+    /**
+     * Get the column name for the "remember me" token.
+     */
+    public function getRememberTokenName() {
+        // not implemented yet
+    }
+
+    /*
+     * Set the token value for the "remember me" session.
+     */
+    public function setRememberToken($value) {
+        // not implemented yet
+    }
 
 }
