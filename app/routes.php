e3476b2ade2a61c1c05dae2319e5ea2d042f31dd
GuggerJoel <joel.gugger@rdlp.ch>
2014-06-20 15:51:02 +0200

    Change Jsend
    Passage des methodes en public sur Jsend

--- app/routes.php
+++ app/routes.php
@@ -138,5 +138,5 @@ Route::group(array('namespace' => 'Rockit\v1', 'prefix' => 'v1'), function()
 
 App::missing(function($exception)
 {
-    return Jsend::compile(array('fail' => array('title' => trans('fail.routes.missing'))));
+    return Jsend::fail('fail.routes.missing');
 });
