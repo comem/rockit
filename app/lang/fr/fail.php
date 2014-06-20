26145bd5fe17a83a593a0f3fa2149733b60baac2
Tazaf <obersonmathias@gmail.com>
2014-06-20 15:52:38 +0200

    début traduction fr

--- app/lang/fr/fail.php
+++ app/lang/fr/fail.php
@@ -1,16 +1,12 @@
 <?php
 
-	/*
-	 * Fail messages
-	*/
+/*
+ * Fail messages
+ */
 
 return array(
-	
-	'language' => array(
-
-		'inexistant' => "L'objet n'existe pas",
-		'missing' => 'La Local est manquante',
-
-	),
-
+    'language' => array(
+        'inexistant' => "La langue n'existe pas",
+        'missing' => "L'attribut 'locale' est manquant",
+    ),
 );
