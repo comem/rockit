221f8ed88a4fafccc11eeb7e621eaca49bee4503
Tazaf <obersonmathias@gmail.com>
2014-06-20 23:19:13 +0200

    starting update methid

--- app/controllers/Rockit/v1/RepresenterController.php
+++ app/controllers/Rockit/v1/RepresenterController.php
@@ -3,67 +3,62 @@
 namespace Rockit\v1;
 
 use \Input,
+    \BaseController,
     Rockit\Representer;
 
-class RepresenterController extends \BaseController {
+class RepresenterController extends BaseController {
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-            $data = Input::only('first_name', 'last_name', 'email', 'phone', 'street', 'npa', 'city');
-            return Representer::validate($data, Representer::$create_rules);
-	}
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function store() {
+        $data = Input::only('first_name', 'last_name', 'email', 'phone', 'street', 'npa', 'city');
+        $response = Representer::validate($data, Representer::$create_rules);
+        if ($response === true) {
+            $response = Representer::createOne($data);
+        }
+        return $response;
+    }
 
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id) {
+        //
+    }
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index() {
+        //
+    }
 
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id) {
+        //
+    }
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		//
-	}
-
-
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
-
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
-
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function update($id) {
+        $data = Input::only('first_name', 'last_name', 'email', 'phone', 'street', 'npa', 'city');
+    }
 
 }
