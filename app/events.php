a99a4ef8cd87107525ecde4c2069312b46e6841c
chrissharkman <ch.sharkman@gmail.com>
2014-06-20 13:43:26 +0200

    Add triggered event

--- app/events.php
+++ app/events.php
@@ -1,12 +1,13 @@
 <?php
 
-Event::listen('auth.login', function($credentials)
-{    
-    $user = User::where('email', '=', $credentials['email'])->first();
-    $dt = Carbon\Carbon::now();
-    $dts = $dt->toDateTimeString();
-    $user->last_login = $dts;
+Event::listen('auth.login', function($user) { 
+//    $user = User::where('email', '=', $credentials['email'])->first();
+//    $dt = Carbon\Carbon::now();
+//    $dts = $dt->toDateTimeString();
+//    $user->last_login = $dts;
+    $user->last_login = new DateTime;
     $user->save();
+   // App::setLocale($user->hasOne('language'));
 });
 
 Event::listen('auth.logout', function() {
