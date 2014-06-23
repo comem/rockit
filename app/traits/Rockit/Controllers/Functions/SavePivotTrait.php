206379441197676823e3ef72b446be2bd7f617e6
Tazaf <obersonmathias@gmail.com>
2014-06-23 22:33:54 +0200

    Revert "puting the right code to the right place..."
    This reverts commit 0c2fd9ac4333e2cc064b7b002212c512b3bf27df.

--- app/traits/Rockit/Controllers/Functions/SavePivotTrait.php
+++ app/traits/Rockit/Controllers/Functions/SavePivotTrait.php
@@ -5,43 +5,23 @@ namespace Rockit\Controllers\Functions;
 trait SavePivotTrait {
 
     /**
-     * Save a new model in the database with the provided valid data.
+     * Save a new pivot in the database between the provided models referenced by their ids.
      * It's possible to check the existence before saving the model by setting the check_existence parameter to "true".
      * If you make it so you have to provide the name of the column upon which the check will be done.
      * The value that this column needs to match will be extract from the data parameter, assuming that its key is the same as the checked column.
      * 
-     * @param string $model The class name of the model you want to save
-     * @param array $data An array containing the data for the model. It is imperative that the keys for these data match the column name in the database.
-     * @param boolean $check_existence Indicate if an existence checking needs to be done
-     * @param string $column The name of the column upon which the check has to be done
+     * @param string $pivot_model The class name of the pivot you want to save
+     * @param array $data An array containing the data for the pivot. It is imperative that the keys of these data match the column name in the database.
      * @return mixed array : An array containing either a 'success', 'fail' or 'error' key depending on the result
      */
-    public static function save($model, array $data, $check_existence = false, $column = 'id') {
-        $call = self::$namespace . $model;
-        if ($check_existence === true) {
-            $object = $call::exist($data[$column], $column);
-            if (is_object($object)) {
-                $response = array('fail' => array(trans('fail.' . snake_case($model) . '.existing')));
-            } else {
-                $response = self::checkForeignKeys($data);
-            }
+    public static function save($pivot_model, array $data) {
+        $call = self::$namespace . $pivot_model;
+        $object = $call::existByIds($data);
+        if (is_object($object)) {
+            $response = array('fail' => trans('fail.' . snake_case($pivot_model) . '.existing'));
         }
         if (!isset($response)) {
             $response = $call::createOne($data);
-        }
-        return $response;
-    }
-
-    protected static function checkForeignKeys($data) {
-        $foreign_keys = array_where($data, function($key) {
-            return ends_with($key, '_id');
-        });
-        $exist_foreign = self::checkForeignKeys($foreign_keys);
-        $response = true;
-        foreach ($foreign_keys as $key => $value) {
-            $model = studly_case(preg_replace('#\_id$#u', '', $key));
-            $call = self::$namespace . $model;
-            $response = $call::exist($value);
         }
         return $response;
     }
