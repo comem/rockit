65a58c9c3a5f4979e11b81806792ea95900715cd
GuggerJoel <joel.gugger@rdlp.ch>
2014-06-24 14:32:58 +0200

    add trait for offer model

--- app/models/Rockit/Offer.php
+++ app/models/Rockit/Offer.php
@@ -2,9 +2,27 @@
 
 namespace Rockit;
 
+use Rockit\Models\CompletePivotModelTrait;
+
 class Offer extends \Eloquent {
 
+	use CompletePivotModelTrait;
+
 	protected $table = 'offers';
+
 	public $timestamps = false;
+	public static $create_rules = [
+		'cost' 			=> 'integer|min:0',
+		'quantity' 		=> 'integer|required|min:1',
+		'comment_de' 	=> 'size:1',
+		'event_id' 		=> 'integer|required|min:1|exists:events,id',
+		'gift_id' 		=> 'integer|required|min:1|exists:gifts,id',
+	];
+	public static $update_rules = [
+		'cost' 			=> 'integer|min:0',
+		'quantity' 		=> 'integer|required|min:1',
+		'comment_de' 	=> 'size:1',
+	];
+	public static $response_field = 'id';
 
 }
