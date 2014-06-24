68eaf29e51f0cd46452bd2cea9c41f4c3e72565d
GuggerJoel <joel.gugger@rdlp.ch>
2014-06-24 14:57:23 +0200

    add trait model needs

--- app/models/Rockit/Need.php
+++ app/models/Rockit/Need.php
@@ -2,9 +2,23 @@
 
 namespace Rockit;
 
+use Rockit\Models\CompletePivotModelTrait;
+
 class Need extends \Eloquent {
 
+	use CompletePivotModelTrait;
+
 	protected $table = 'needs';
+
 	public $timestamps = false;
+	public static $create_rules = [
+		'nb_people' 	=> 'integer|required|min:1',
+		'event_id' 		=> 'integer|required|min:1|exists:events,id',
+		'skill_id' 		=> 'integer|required|min:1|exists:skills,id',
+	];
+	public static $update_rules = [
+		'nb_people' 	=> 'integer|required|min:1',
+	];
+	public static $response_field = 'id';
 
 }
