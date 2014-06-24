85a1d40c69ddb2ff762f72bde5951bf566c3ac61
robertdirosa <robert@di-rosa.ch>
2014-06-24 11:28:31 +0200

    SF Instruments

--- app/controllers/Rockit/v1/InstrumentController.php
+++ app/controllers/Rockit/v1/InstrumentController.php
@@ -3,67 +3,49 @@
 namespace Rockit\v1;
 
 use Rockit\Instrument;
-
-use \App, \Lang, \Input, \Auth, \Jsend;
+use \App,
+    \Lang,
+    \Input,
+    \Auth,
+    \Jsend;
 
 class InstrumentController extends \BaseController {
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		 return  \Jsend::success(\Rockit\Instrument::all()->toArray());
-	}
+    use \Rockit\Controllers\ControllerBSRDTrait;
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index() {
+        return \Jsend::success(\Rockit\Instrument::all()->toArray());
+    }
 
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function store() {
+        $data = \Input::only('name_de');
+        $response = self::renew('Instrument', $data);
+        if ($response === false) {
+            $response = \Rockit\Instrument::validate($data, \Rockit\Instrument::$create_rules);
+            if ($response === true) {
+                $response = self::save('Instrument', $data, TRUE, 'name_de');
+            }
+        }
+        return \Jsend::compile($response);
+    }
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		$inputs = Input::only('name_de');
-		$validate = Instrument::validate( $inputs, Instrument::$create_rules );
-		if( $validate === true ){
-			$response = self::setLocale( $inputs['name_de'] );
-		} else {
-			$response = $validate;
-		}
-		return Jsend::compile($response);
-	}
-
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		$name_de = Instrument::exist( $id );
-		if( is_object( $name_de ) ) {
-			/*
-			Lang::where()
-			$user = Auth::user();
-			$user->language_id = 0;
-			*/
-			//$response['success'] = $lang;
-			$response = Instrument::deleteOne(
-				$name_de
-			);
-
-			return $response;
-
-			//return $lang;
-		} else {
-			return $name_de;
-		}
-	}
-
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id) {
+        return \Jsend::compile(self::delete('Instrument', $id));
+    }
 
 }
--- app/models/Rockit/Instrument.php
+++ app/models/Rockit/Instrument.php
@@ -2,10 +2,15 @@
 
 namespace Rockit;
 
-use \Validator;
-
 class Instrument extends \Eloquent {
 
+        use \Illuminate\Database\Eloquent\SoftDeletingTrait,
+        Models\ModelBCRDTrait;
+        
+        public static $create_rules = array(
+            'name_de' => 'required',
+        );
+    
 	protected $table = 'instruments';
 	public $timestamps = false;
 
@@ -13,88 +18,4 @@ class Instrument extends \Eloquent {
 	{
 		return $this->hasMany('Rockit\Lineup');
 	}
-        
-        public static $create_rules = array(
-		'name_de' => 'alpha|required|min:1',
-	);
-
-	public static $update_rules = array(
-		'name_de' => 'alpha|required|min:1',
-	);
-
-	public static function exist( $name )
-	{
-		$response = self::where('name_de', '=', $name)->first();
-		if($response == NULL){
-			$response['fail'] = trans('fail.instrument.inexistant');
-		}
-		return $response;
-	}
-
-	public static function validate( $inputs, $rules )
-	{
-		$v = Validator::make( $inputs, $rules );
-		if( $v->fails() ){
-			$response['fail'] = $v->messages()->getMessages();
-		} else {
-			$response = true;
-		}
-		return $response;
-	}
-
-	public static function createOne( $inputs )
-	{
-		self::unguard();
-		$object = self::create( $inputs );
-		if( $object != null ){
-			$response['success'] = array(
-				'title' => trans('success.instrument.created'),
-				'id' => $object->id,
-			);
-		} else {
-			$response['error'] = trans('error.instrument.created');
-		}
-		return $response;
-	}
-
-	public static function updateOne( $new_values, Instrument $object )
-	{
-		foreach( $new_values as $key => $value )
-		{
-			$object->$key = $value;
-		}
-		if( $object->save() ){ 
-			$response['success'] = array(
-				'title' => trans('success.instrument.updated'),
-			);
-		} else {
-			$response['error'] = trans('error.instrument.updated');
-		}
-		return $response;
-	}
-
-	public static function deleteOne(Instrument $object )
-	{
-		if( $object->delete() ){ 
-			$response['success'] = array(
-				'title' => trans('success.instrument.deleted'),
-			);
-		} else {
-			$response['error'] = trans('error.linstrument.deleted');
-		}
-		return $response;
-	}
-
-	public static function restoreOne(Instrument $object )
-	{
-		if( $object->restore() ){ 
-			$response['success'] = array(
-				'title' => trans('success.instrument.restored'),
-			);
-		} else {
-			$response['error'] = trans('error.instrument.restored');
-		}
-		return $response;
-	}
-
 }
