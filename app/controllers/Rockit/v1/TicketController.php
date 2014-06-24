13b2218b128e460f30873d0c2d2e8c4f5ad0ba1b
GuggerJoel <joel.gugger@rdlp.ch>
2014-06-24 15:19:38 +0200

    add trait for ticket (model + controller)

--- app/controllers/Rockit/v1/TicketController.php
+++ app/controllers/Rockit/v1/TicketController.php
@@ -2,8 +2,14 @@
 
 namespace Rockit\v1;
 
+use \Input,
+    \Jsend,
+    \Rockit\Ticket,
+	Rockit\Controllers\CompletePivotControllerTrait;
+
 class TicketController extends \BaseController {
 
+	use CompletePivotControllerTrait;
 
 	/**
 	 * Store a newly created resource in storage.
@@ -12,7 +18,12 @@ class TicketController extends \BaseController {
 	 */
 	public function store()
 	{
-		//
+		$data = Input::only('amount', 'quantity_sold', 'comment_de', 'event_id', 'ticket_category_id');
+		$response = Ticket::validate($data, Ticket::$create_rules);
+        if ($response === true) {
+            $response = self::save('Ticket', $data);
+        }
+        return Jsend::compile($response);
 	}
 
 
@@ -24,7 +35,12 @@ class TicketController extends \BaseController {
 	 */
 	public function update($id)
 	{
-		//
+		$data = Input::only('amount', 'quantity_sold', 'comment_de');
+		$response = Ticket::validate($data, Ticket::$update_rules);
+        if ($response === true) {
+            $response = self::modify('Ticket', $id, $data);
+        }
+        return Jsend::compile($response);
 	}
 
 
@@ -36,7 +52,7 @@ class TicketController extends \BaseController {
 	 */
 	public function destroy($id)
 	{
-		//
+		return Jsend::compile(self::delete('Ticket', $id));
 	}
 
 
--- app/models/Rockit/Ticket.php
+++ app/models/Rockit/Ticket.php
@@ -2,9 +2,27 @@
 
 namespace Rockit;
 
+use Rockit\Models\CompletePivotModelTrait;
+
 class Ticket extends \Eloquent {
 
+	use CompletePivotModelTrait;
+
 	protected $table = 'tickets';
+
 	public $timestamps = false;
+	public static $create_rules = [
+		'amount' 			=> 'integer|required|min:0',
+		'quantity_sold'		=> 'integer|min:0',
+		'comment_de' 		=> 'min:1',
+		'event_id' 			=> 'integer|required|min:1|exists:events,id',
+		'ticket_catgory_id'	=> 'integer|required|min:1|exists:ticket_categories,id',
+	];
+	public static $update_rules = [
+		'amount' 			=> 'integer|min:0',
+		'quantity_sold'		=> 'integer|min:0',
+		'comment_de' 		=> 'min:1',
+	];
+	public static $response_field = 'id';
 
 }
