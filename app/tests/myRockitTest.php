bcac941bc999ca6b2dd6c1ee3f8394f12dad1565
chrisdeguzman <chnky13@gmail.com>
2014-06-25 17:55:43 +0200

    attempted third testest
    composite t

--- app/tests/myRockitTest.php
+++ app/tests/myRockitTest.php
@@ -2,31 +2,63 @@
 
 class myRockitTest extends TestCase {
 
+	/////////////////////////////// Single test ////////////////////////////////////
+
 	/**
 	 * A basic functional test example.
 	 *
 	 * @return void
 	 */
-	public function testGetGenreById()
+	public function testById()
 	{
-			
-		$response = $this->call('GET', 'v1/members', array('id' => 3));
-	// $response = $this->call('GET', 'MemberController@show', array('id' => '3')); // doesnt work
+		var_dump($id);
+		$response = $this->call('GET', 'v1/members', array('id' => 2));
 
 	var_dump($response->getContent() . ' is received');
 	// receive a string
-	$this->assertEquals(3, $response->getContent());
+	$this->assertEquals(2, $response->getContent(),'We expected to see that the id sent = 2');
 	// receive a string
 	// $this->assertEquals("true", $response->getContent());
 
 	// $this->assertResponseOk();
 	}
 
+	/////////////////////////// Composite Test ///////////////////////////////////
 
-	public function testLangs()
-	{
-		$response = $this->call('GET', 'v1/langs');
-		var_dump($response->getContent());
+	public function testMultipleIds() {
+		$this->testGetMemberById('1');
+		$this->testGetMemberById('2');
+		// $this->testGetMemberById(3);
 	}
+
+
+	/**
+	 * A basic functional test example.
+	 *
+	 * @return void
+	 */
+	protected function testGetMemberById($id)
+	{
+		var_dump($id);
+		$response = $this->call('GET', 'v1/members', array('id' => {$id}));
+	// $response = $this->call('GET', 'MemberController@show', array('id' => '3')); // doesnt work
+
+	var_dump($response->getContent() . ' is received');
+	// receive a string
+	$this->assertEquals(2, $response->getContent(),'We expected to see that {$id} sent = 2');
+	// receive a string
+	// $this->assertEquals("true", $response->getContent());
+
+	// $this->assertResponseOk();
+	}
+
+	///////////////////////////// PDO exception test ////////////////////////////
+
+	// // why is this taking so long?
+	// public function testLangs()
+	// {
+	// 	$response = $this->call('GET', 'v1/langs');
+	// 	var_dump($response->getContent());
+	// }
 
 }
