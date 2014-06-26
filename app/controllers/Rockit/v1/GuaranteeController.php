67ea962854d0aa122d925ff3400998d0e399ea2b
GuggerJoel <joel.gugger@rdlp.ch>
2014-06-26 19:29:18 +0200

    add save sa on guarantee

--- app/controllers/Rockit/v1/GuaranteeController.php
+++ app/controllers/Rockit/v1/GuaranteeController.php
@@ -2,6 +2,8 @@
 
 namespace Rockit\v1;
 
+use \Input, \Validator, \Rockit\Image, \Jsend;
+
 class GuaranteeController extends \BaseController {
 
 	/**
@@ -11,7 +13,18 @@ class GuaranteeController extends \BaseController {
 	 */
 	public function store()
 	{
-		//
+		$inputs = Input::only('representer_id', 'event_id');
+		$v = Validator::make(
+		    $inputs,
+		    ['representer_id' => 'required|exists:representers,id',
+		    'event_id' => 'required|exists:events,id']
+		);
+		if($v->passes()){
+			$response = self::save( $inputs );
+		} else {
+			$response['fail'] = $v->messages()->getMessages();
+		}
+		return Jsend::compile($response);
 	}
 
 
