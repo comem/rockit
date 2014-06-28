70a367b18c95e5cd546e45ff188244fa0aa26565
Tazaf <obersonmathias@gmail.com>
2014-06-28 14:42:04 +0200

    Reordered error.php

--- app/lang/fr/error.php
+++ app/lang/fr/error.php
@@ -5,11 +5,85 @@
  */
 
 return [
+    'app' => "Une erreur est survenue (code :code). Merci de contacter un administrateur.",
+    'attribution' => [
+        'created' => "L'attribution n'a pas pu être créée",
+        'updated' => "L'attribution n'a pas pu être modifiée",
+        'deleted' => "L'attribution n'a pas pu être supprimée",
+    ],
+    'auth' => [
+        'login' => "L'authentification a échouée.",
+        'logout' => "La déconnexion a échouée.",
+    ],
+    'description' => [
+        'created' => "La description n'a pas pu être enregistrée",
+        'deleted' => "La description n'a pas pu être supprimée",
+    ],
+    'event_type' => [
+        'created' => "Le type d'événement n'a pas pu être créé",
+        'deleted' => "Le type d'événement n'a pas pu être supprimé",
+        'restored' => "Le type d'événement n'a pas pu être restauré",
+    ],
+    'file' => [
+        'not_deleted' => "Le fichier \":file\" n'a pas pu être supprimé.",
+        'not_uploaded' => "Le fichier \":file\" n'a pas pu être téléchargé.",
+    ],
+    'fulfillment' => [
+        'created' => "La compétence n'a pas pu être assignée",
+        'deleted' => "La compétence assignée n'a pas pu être supprimée",
+    ],
+    'gift' => [
+        'created' => "Le lot n'a pas pu être créé",
+        'deleted' => "Le lot n'a pas pu être supprimé",
+        'restored' => "Le lot n'a pas pu être restauré",
+    ],
+    'guarantee' => [
+        'created' => "Le représentant n'a pas pu être rajouté à l'événement.",
+        'deleted' => "Le représentant n'a pas pu être supprimé à l'événement.",
+    ],
+    'illustration' => [
+        'created' => "L'image n'a pas pu être rajoutée à l'artiste.",
+        'deleted' => "L'image n'a pas pu être supprimée à l'artiste.",
+        'inexistant' => "Il n'y aucune relation pour cette image.",
+    ],
+    'instrument' => [
+        'created' => "L instrument n'a pas pu être supprimé",
+        'restored' => "L instrument n'a pas pu être restauré",
+    ],
     'language' => [
         'created' => "La langue n'a pas pu être créée.",
         'updated' => "La langue n'a pas pu être modifiée.",
         'deleted' => "La langue n'a pas pu être supprimée.",
         'restored' => "La langue n'a pas pu être restaurée.",
+    ],
+    'lineup' => [
+        'created' => "La formation n'a pas pu être enregistrée",
+        'deleted' => "La formation n'a pas pu être supprimée",
+    ],
+    'need' => [
+        'created' => "Le besoin n'a pas pu être créé",
+        'deleted' => "Le besoin n'a pas pu être supprimé",
+        'restored' => "Le besoin n'a pas pu être restauré",
+    ],
+    'offer' => [
+        'created' => "L'offre n'a pas pu être créée",
+        'updated' => "L'offre n'a pas pu être modifiée",
+        'deleted' => "L'offre n'a pas pu être supprimée",
+    ],
+    'performer' => [
+        'created' => "La liaison n'a pas pu être créée",
+        'updated' => "La liaison n'a pas pu être modifiée",
+        'deleted' => "La liaison n'a pas pu être supprimée",
+    ],
+    'printing' => [
+        'created' => "L'imprimé n'a pas pu être créée",
+        'updated' => "L'imprimé n'a pas pu être modifiée",
+        'deleted' => "L'imprimé n'a pas pu être supprimée",
+    ],
+    'printing_type' => [
+        'created' => "Le type d'imprimé n'a pas pu être créé",
+        'deleted' => "Le type d'imprimé n'a pas pu être supprimé",
+        'restored' => "Le type d'imprimé n'a pas pu être restauré",
     ],
     'representer' => [
         'created' => "Le représentant n'a pas pu être créé.",
@@ -17,106 +91,28 @@ return [
         'deleted' => "Le représentant n'a pas pu être supprimé.",
         'restored' => "Le représentant n'a pas pu être restauré.",
     ],
-    'auth' => [
-        'login' => "L'authentification a échouée.",
-        'logout' => "La déconnexion a échouée.",
-    ],
-    'instrument' => [
-        'created' => "L instrument n'a pas pu être supprimé",
-        'restored' => "L instrument n'a pas pu être restauré",
-    ],
-    'gift' => [
-        'created' => "Le lot n'a pas pu être créé",
-        'deleted' => "Le lot n'a pas pu être supprimé",
-        'restored' => "Le lot n'a pas pu être restauré",
-    ],
-    'event_type' => [
-        'created' => "Le type d'événement n'a pas pu être créé",
-        'deleted' => "Le type d'événement n'a pas pu être supprimé",
-        'restored' => "Le type d'événement n'a pas pu être restauré",
-    ],
-    'attribution' => [
-        'created' => "L'attribution n'a pas pu être créée",
-        'updated' => "L'attribution n'a pas pu être modifiée",
-        'deleted' => "L'attribution n'a pas pu être supprimée",
-    ],
-    'offer' => [
-        'created' => "L'offre n'a pas pu être créée",
-        'updated' => "L'offre n'a pas pu être modifiée",
-        'deleted' => "L'offre n'a pas pu être supprimée",
-    ],
-    'printing_type' => [
-        'created' => "Le type d'imprimé n'a pas pu être créé",
-        'deleted' => "Le type d'imprimé n'a pas pu être supprimé",
-        'restored' => "Le type d'imprimé n'a pas pu être restauré",
-    ],
-    'ticket_category' => [
-        'created' => "La catégorie de tickets n'a pas pu être créé",
-        'deleted' => "La catégorie de tickets n'a pas pu être supprimé",
-        'restored' => "La catégorie de tickets  n'a pas pu être restauré",
-    ],
-    'ticket' => [
-        'created' => "Le ticket d'entrée n'a pas pu être créé",
-        'deleted' => "Le ticket d'entrée n'a pas pu être supprimé",
-        'restored' => "Le ticket d'entrée n'a pas pu être restauré",
-    ],
-    'need' => [
-        'created' => "Le besoin n'a pas pu être créé",
-        'deleted' => "Le besoin n'a pas pu être supprimé",
-        'restored' => "Le besoin n'a pas pu être restauré",
+    'staff' => [
+        'created' => "Le role n'a pas pu être assigné",
+        'updated' => "Le role n'a pas pu être modifié",
+        'deleted' => "Le role n'a pas pu être supprimé",
     ],
     'skill' => [
         'created' => "La compétence n'a pas pu être créé",
         'deleted' => "La compétence n'a pas pu être supprimé",
         'restored' => "La compétence n'a pas pu être restauré",
     ],
-    'fulfillment' => [
-        'created' => "La compétence n'a pas pu être assignée",
-        'deleted' => "La compétence assignée n'a pas pu être supprimée",
-    ],
-    'staff' => [
-        'created' => "Le role n'a pas pu être assigné",
-        'updated' => "Le role n'a pas pu être modifié",
-        'deleted' => "Le role n'a pas pu être supprimé",
-    ],
-    'lineup' => [
-        'created' => "La formation n'a pas pu être enregistrée",
-        'deleted' => "La formation n'a pas pu être supprimée",
-    ],
-    'description' => [
-        'created' => "La description n'a pas pu être enregistrée",
-        'deleted' => "La description n'a pas pu être supprimée",
-    ],
-    'printing' => [
-        'created' => "L'imprimé n'a pas pu être créée",
-        'updated' => "L'imprimé n'a pas pu être modifiée",
-        'deleted' => "L'imprimé n'a pas pu être supprimée",
-    ],
-    'performer' => [
-        'created' => "La liaison n'a pas pu être créée",
-        'updated' => "La liaison n'a pas pu être modifiée",
-        'deleted' => "La liaison n'a pas pu être supprimée",
-    ],
-    'illustration' => [
-        'created' => "L'image n'a pas pu être rajoutée à l'artiste.",
-        'deleted' => "L'image n'a pas pu être supprimée à l'artiste.",
-    ],
     'symbolization' => [
         'created' => "L'image n'a pas pu être rajoutée à l'événement.",
         'deleted' => "L'image n'a pas pu être supprimée à l'événement.",
     ],
-    'illustration' => [
-        'created' => "Le représentant n'a pas pu être rajouté à l'événement.",
-        'deleted' => "Le représentant n'a pas pu être supprimé à l'événement.",
+    'ticket' => [
+        'created' => "Le ticket d'entrée n'a pas pu être créé",
+        'deleted' => "Le ticket d'entrée n'a pas pu être supprimé",
+        'restored' => "Le ticket d'entrée n'a pas pu être restauré",
     ],
-    'guarantee' => [
-        'created' => "Le représentant n'a pas pu être rajouté à l'événement.",
-        'deleted' => "Le représentant n'a pas pu être supprimé à l'événement.",
+    'ticket_category' => [
+        'created' => "La catégorie de tickets n'a pas pu être créé",
+        'deleted' => "La catégorie de tickets n'a pas pu être supprimé",
+        'restored' => "La catégorie de tickets  n'a pas pu être restauré",
     ],
-    'inexistant' => "Il n'y aucune relation pour cette image.",
-    'file' => [
-        'not_deleted' => "Le fichier \":file\" n'a pas pu être supprimé.",
-        'not_uploaded' => "Le fichier \":file\" n'a pas pu être téléchargé.",
-    ],
-    'app' => "Une erreur est survenue (code :code). Merci de contacter un administrateur.",
 ];
