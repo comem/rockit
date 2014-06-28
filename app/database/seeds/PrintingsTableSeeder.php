e4d95b49f974b6d9e2b0eb61bb24536ee37ce725
Tazaf <obersonmathias@gmail.com>
2014-06-28 17:28:53 +0200

    updated migrations and seed

--- app/database/seeds/PrintingsTableSeeder.php
+++ app/database/seeds/PrintingsTableSeeder.php
@@ -15,79 +15,79 @@ use Rockit\Event;
          DB::statement('SET FOREIGN_KEY_CHECKS = 0');
          DB::table('printings')->delete();
 
-         Printing::create(array('source' => 'die band',
+         Printing::create(array('source' => 'printings/die_band2.pdf',
                                  'nb_copies'=>'30',
                                  'nb_copies_surplus'=>'40',
                                  'event_id' => $event[0]->id,
                                  'printing_type_id' => $printingtype[0]->id));
          
-         Printing::create(array('source' => 'die band',
+         Printing::create(array('source' => 'printings/die_band.pdf',
                                  'nb_copies'=>'20',
                                  'nb_copies_surplus'=>'1',
                                  'event_id' => $event[2]->id,
                                  'printing_type_id' => $printingtype[1]->id));
          
-         Printing::create(array('source' => 'ein supporter',
+         Printing::create(array('source' => 'printings/ein_supporter.pdf',
                                  'nb_copies'=>'20',
                                  'nb_copies_surplus'=>'4',
                                  'event_id' => $event[2]->id,
                                  'printing_type_id' => $printingtype[2]->id));
          
-         Printing::create(array('source' => 'ptinting/slsamia.pdf',
+         Printing::create(array('source' => 'printings/slsamia.pdf',
                                  'nb_copies'=>'8',
                                  'nb_copies_surplus'=>'8',
                                  'event_id' =>'4',
                                  'printing_type_id' => '4'));
          
-         Printing::create(array('source' => 'printing/salsamia2.pdf',
+         Printing::create(array('source' => 'printings/salsamia2.pdf',
                                  'nb_copies'=>'10',
                                  'nb_copies_surplus'=>'4',
                                  'event_id' => '4',
                                  'printing_type_id' => '1'));
          
-         Printing::create(array('source' => 'printing/unser_Flyer.pdf',
+         Printing::create(array('source' => 'printings/unser_Flyer.pdf',
                                  'nb_copies'=>'8',
                                  'nb_copies_surplus'=>'4',
                                  'event_id' => '5',
                                  'printing_type_id' => '4'));
          
-         Printing::create(array('source' => 'printing/unserPlkart.pdf',
+         Printing::create(array('source' => 'printings/unserPlkart.pdf',
                                  'nb_copies'=>'10',
                                  'nb_copies_surplus'=>'4',
                                  'event_id' =>'5',
                                  'printing_type_id' => '1'));
          
-         Printing::create(array('source' => 'printing/bluedög.philipp.1.pdf',
+         Printing::create(array('source' => 'printings/bluedög.philipp.1.pdf',
                                  'nb_copies'=>'8',
                                  'nb_copies_surplus'=>'0',
                                  'event_id' => '6',
                                  'printing_type_id' => '4'));
          
-         Printing::create(array('source' => 'printing/bluedög.philipp.2.pdf',
+         Printing::create(array('source' => 'printings/bluedög.philipp.2.pdf',
                                  'nb_copies'=>'10',
                                  'nb_copies_surplus'=>'0',
                                  'event_id' => '6',
                                  'printing_type_id' => '1'));
          
-        Printing::create(array('source' => 'printing/konsi-flyer.pdf',
+        Printing::create(array('source' => 'printings/konsi-flyer.pdf',
                                  'nb_copies'=>'8',
                                  'nb_copies_surplus'=>'0',
                                  'event_id' => '7',
                                  'printing_type_id' => '4'));
          
-         Printing::create(array('source' => 'printing/konsi-plakart.pdf',
+         Printing::create(array('source' => 'printings/konsi-plakart.pdf',
                                  'nb_copies'=>'10',
                                  'nb_copies_surplus'=>'0',
                                  'event_id' => '7',
                                  'printing_type_id' => '1'));
          
-         Printing::create(array('source' => 'printing/pigeon.dox',
+         Printing::create(array('source' => 'printings/pigeon.dox',
                                  'nb_copies'=>'8',
                                  'nb_copies_surplus'=>'0',
                                  'event_id' => '8',
                                  'printing_type_id' => '4'));
          
-         Printing::create(array('source' => 'printing/pigeonpigeon.docx',
+         Printing::create(array('source' => 'printings/pigeonpigeon.docx',
                                  'nb_copies'=>'10',
                                  'nb_copies_surplus'=>'0',
                                  'event_id' => '8',
