89621d36bd70c5289314da4dcf4beda61a47a968
Tazaf <obersonmathias@gmail.com>
2014-06-30 15:22:36 +0200

    Resolved PrintingTypesSeeder suppression

---
+++ app/database/seeds/PrintingsTypesTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Rockit\PrintingType;
+
+//printings_types
+class PrintingsTypesTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('printing_types')->delete();
+
+        PrintingType::create(array('id'=>'1',
+            'name_de' => 'flyer'));
+        
+        PrintingType::create(array('id'=>'2',
+            'name_de' => 'Plakart'));
+        
+        PrintingType::create(array('id'=>'3',
+            'name_de' => 'Postkarte'));
+        
+        PrintingType::create(array('id'=>'4',
+            'name_de' => 'Plakart gross'));
+    }
+}
+
