c008bc2927dffe2f018478c1401d5424d34d528f
GuggerJoel <joel.gugger@rdlp.ch>
2014-07-01 19:57:08 +0200

    add SearchController

---
+++ app/controllers/Rockit/v1/SearchController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Rockit\v1;
+
+use \Jsend;
+
+/**
+ * 
+ * @author Joël Gugger <joel.gugger@heig-vd.ch>
+ */
+class ArtistController extends \BaseController {
+
+
+
+    /**
+     * Display a listing of the resource.
+     * 
+     * 
+     * 
+     * @return Jsend
+     */
+    public function index() {
+        //
+    }
+
+
+}
