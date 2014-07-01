7526665dd274ca7c700fd05810c516f13a8ed7ec
chrissharkman <ch.sharkman@gmail.com>
2014-07-01 13:49:27 +0200

    Change config to database data, add elements to platformTableSeeder

--- app/controllers/Rockit/v1/FacebookController.php
+++ app/controllers/Rockit/v1/FacebookController.php
@@ -9,6 +9,7 @@ use Facebook\FacebookRedirectLoginHelper,
     Facebook\FacebookRequestException,
     Facebook\GraphUser,
     Rockit\Sharing,
+    Rockit\Platform,
     Rockit\Event,
     \Facebook,
     \Config,
@@ -21,16 +22,18 @@ class FacebookController extends \BaseController {
 
     /**
      * This function handles the Facebook login: it directs on fb login mask. 
-     * After a successful login of facebook, it redirects on the given url.
-     * @return Redirection to redirect url
+     * After a successful login of facebook, it redirects on the given url (taken from database).
+     * @return Redirection to redirect_url
      */
     public static function login() {
         session_start();
 
-        $redirectUrl = Config::get('facebook.redirecturl'); // will be taken from Database
-        $appkey = Config::get('facebook.appkey');
-        $secretkey = Config::get('facebook.secretkey');
-        $scope = Config::get('facebook.scope');
+        $platform = Platform::where('name', '=', 'facebook')->first();
+        $secretkey = $platform->client_secret;
+        $api_infos = json_decode($platform->api_infos);
+        $redirectUrl = $api_infos->redirect_url;
+        $appkey = $api_infos->appkey;
+        $scope = $api_infos->scope;
         FacebookSession::setDefaultApplication($appkey, $secretkey);
 
         $helper = new FacebookRedirectLoginHelper($redirectUrl);
@@ -48,10 +51,11 @@ class FacebookController extends \BaseController {
     public function redirectCatch() {
         session_start();
 
-        $redirectUrl = Config::get('facebook.redirecturl'); // will be taken from Database
-        $appkey = Config::get('facebook.appkey');
-        $secretkey = Config::get('facebook.secretkey');
-        $scope = Config::get('facebook.scope');
+        $platform = Platform::where('name', '=', 'facebook')->first();
+        $secretkey = $platform->client_secret;
+        $api_infos = json_decode($platform->api_infos);
+        $redirectUrl = $api_infos->redirect_url;
+        $appkey = $api_infos->appkey;
         FacebookSession::setDefaultApplication($appkey, $secretkey);
 
         $helper = new FacebookRedirectLoginHelper($redirectUrl);
@@ -70,10 +74,10 @@ class FacebookController extends \BaseController {
         if (isset($response['error'])) {
             return $response;
         }
-        if(Session::get('fb_share')) {
-            return Jsend::compile(self::shareEvent(Session::get('event_id')));
+        if (Session::get('fb_share')) {
+            return Jsend::compile(self::shareEvent());
         } elseif (Session::get('fb_delete')) {
-            return Jsend::compile(self::deleteEvent(Session::get('external_id')));
+            return Jsend::compile(self::deleteEvent());
         } else {
             return Jsend::error(trans('error.facebook.noaction'));
         }
@@ -87,84 +91,98 @@ class FacebookController extends \BaseController {
      * @return $response with success or error
      * @author Christian Heimann <christian.heimann@heig-vd.ch>
      */
-    public static function shareEvent($event_id) {
+    public static function shareEvent() {
         $session = Session::get('facebookSession');
-        $additionalText = Session::get('additional_text');
-        if(is_null($additionalText)) {
-            $additionalText = "";
-        }
-        $event = Event::find($event_id);
+        $event_id = Session::get('event_id');
+        $platform = Platform::where('name', '=', 'facebook')->first();
+        $api_infos = json_decode($platform->api_infos);
 
-        $message = Sharing::message($event, $additionalText);
-        
-        if ($session) {
-            try {
-                $user_profile = (new FacebookRequest(
-                        $session, 'GET', '/me'
-                        ))->execute()->getGraphObject(GraphUser::className());
-            } catch (FacebookRequestException $e) {
-                $response['error'] = trans('error.facebook.userprofile');
-            }
+        $additional_text = Session::get('additional_text');
+        if (is_null($additional_text)) {
+            $additional_text = "";
         }
-        // check if it is the correct user – cause everyone can log itself,
-        // but only the user in config can delete post automatically
-        if (isset($user_profile) && strcasecmp($user_profile->getName(), Config::get('facebook.username')) == 0) {
-            // create a post
+        if (Event::exist($event_id)) {
+            $message = Sharing::message($event, $additional_text);
+
             if ($session) {
                 try {
-                    $fbResponse = (new FacebookRequest(
-                            $session, 'POST', '/me/feed', array(
+                    $user_profile = (new FacebookRequest(
+                            $session, 'GET', '/me'
+                            ))->execute()->getGraphObject(GraphUser::className());
+                } catch (FacebookRequestException $e) {
+                    $response['error'] = trans('error.facebook.userprofile');
+                }
+            }
+            // check if it is the correct user – cause everyone can log itself,
+            // but only the user in config can delete post automatically
+            if (isset($user_profile) && strcasecmp($user_profile->getName(), $api_infos->username) == 0) {
+                // create a post
+                if ($session) {
+                    try {
+                        $fbResponse = (new FacebookRequest(
+                                $session, 'POST', '/me/feed', array(
                             'message' => $message,
-                            'place' => Config::get('facebook.placeid'),
-                            'link' => "www.mahogany.ch"
-                            )
-                            ))->execute()->getGraphObject();
-                } catch (FacebookRequestException $e) {
-                    $response['error'] = "Exception occured, code: " . $e->getCode() . " with message: " . $e->getMessage();
+                            'place' => $api_infos->place_id,
+                            'link' => $api_infos->link,
+                                )
+                                ))->execute()->getGraphObject();
+                    } catch (FacebookRequestException $e) {
+                        $response['error'] = "Exception occured, code: " . $e->getCode() . " with message: " . $e->getMessage();
+                    }
+                    // if no error occured, set inputs for creation of a new sharing
+                    if (!isset($response['error'])) {
+                        $inputs['external_id'] = $fbResponse->getProperty('id');
+                        $inputs['url'] = "www.facebook.com";
+                        $inputs['platform_id'] = Session::get('platform_id');
+                        $inputs['event_id'] = Session::get('event_id');
+                        $inputs['external_infos'] = json_encode(['additional_text' => $additionalText]);
+                        $response = Sharing::createOne($inputs);
+                    }
                 }
-                // if no error occured, set inputs for creation of a new sharing
-                if (!isset($response['error'])) {
-                    $inputs['external_id'] = $fbResponse->getProperty('id');
-                    $inputs['url'] = "www.facebook.com";
-                    $inputs['platform_id'] = Session::get('platform_id');
-                    $inputs['event_id'] = Session::get('event_id');
-                    $inputs['external_infos'] = json_encode(['additional_text' => $additionalText]);
-                    $response = Sharing::createOne($inputs);
-                }
+            } elseif (!isset($response['error'])) {
+                $response['error'] = trans('error.facebook.wronguser');
             }
-        } elseif (!isset($response['error'])) {
-            $response['error'] = trans('error.facebook.wronguser');
+        } else {
+            $response['fail'] = [
+                'event' => trans('fail.event.inexistant'),
+            ];
         }
         return $response;
     }
-    
-    public static function deleteEvent($externalId) {
+
+    public static function deleteEvent() {
         $session = Session::get('facebookSession');
-        if ($session) {
-            try {
-                $user_profile = (new FacebookRequest(
-                        $session, 'GET', '/me'
-                        ))->execute()->getGraphObject(GraphUser::className());
-            } catch (FacebookRequestException $e) {
-                $response['error'] = trans('error.facebook.userprofile');
-            }
-        }
-        if (isset($user_profile) && strcasecmp($user_profile->getName(), Config::get('facebook.username')) == 0) {
-            // delete a post
+        $external_id = Session::get('external_id');
+        $platform = Platform::where('name', '=', 'facebook')->first();
+        $api_infos = json_decode($platform->api_infos);
+
+        if (Sharing::exist($external_id)) {
             if ($session) {
                 try {
-                    $fbResponse = (new FacebookRequest(
-                            $session, 'DELETE', '/' . $externalId))->execute()->getGraphObject();
+                    $user_profile = (new FacebookRequest(
+                            $session, 'GET', '/me'
+                            ))->execute()->getGraphObject(GraphUser::className());
                 } catch (FacebookRequestException $e) {
-                    $response['error'] = "Exception occured, code: " . $e->getCode() . " with message: " . $e->getMessage();
-                }
-                if(!isset($response['error'])) {
-                    $sharing = Session::get('sharing');
-                    $response = Sharing::deleteOne($sharing);
+                    $response['error'] = trans('error.facebook.userprofile');
                 }
             }
-        } 
+            if (isset($user_profile) && strcasecmp($user_profile->getName(), $api_infos->username) == 0) {
+                // delete a post
+                if ($session) {
+                    try {
+                        $fbResponse = (new FacebookRequest(
+                                $session, 'DELETE', '/' . $external_id))->execute()->getGraphObject();
+                    } catch (FacebookRequestException $e) {
+                        $response['error'] = "Exception occured, code: " . $e->getCode() . " with message: " . $e->getMessage();
+                    }
+                    if (!isset($response['error'])) {
+                        $sharing = Session::get('sharing');
+                        $response = Sharing::deleteOne($sharing);
+                    }
+                }
+            }
+        }
         return $response;
     }
-    
+
 }
