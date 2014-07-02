31b7c755bee8aa40d6b98e9d2067daabc5ee3af1
chrisdeguzman <chnky13@gmail.com>
2014-07-02 09:31:02 +0200

    copy all lang from french to english folder

--- app/lang/en/error.php
+++ app/lang/en/error.php
@@ -4,21 +4,171 @@
  * Error messages
  */
 
-return array(
-    'language' => array(
-        'created' => "La langue n'a pas pu être créée.",
-        'updated' => "La langue n'a pas pu être modifiée.",
-        'deleted' => "La langue n'a pas pu être supprimée.",
-        'restored' => "La langue n'a pas pu être restaurée.",
-    ),
-    'representer' => array(
-        'created' => "Le représentant n'a pas pu être créé.",
-        'updated' => "Le représentant n'a pas pu être modifié.",
-        'deleted' => "Le représentant n'a pas pu être supprimé.",
-        'restored' => "Le représentant n'a pas pu être restauré.",
-    ),
-    'auth' => array(
-        'login' => "L'authentification a échouée.",
-        'logout' => "La déconnexion a échouée.",
-    ),
-);
+return [
+    'app' => "Une erreur est survenue (code :code). Merci de contacter un administrateur.",
+    'artist' => [
+        'updated' => "La mise à jour de l'artiste \":name\" a échoué.",
+        'created' => "La création de l'artiste \":name\" a échoué.",
+        'deleted' => "La suppression de l'artiste \":name\" a échoué.",
+        'restored' => "La restauration de l'artiste \":name\" a échoué.",
+    ],
+    'attribution' => [
+        'created' => "La création de l'attribution a échoué.",
+        'updated' => "La mise à jour de l'attribution a échoué.",
+        'deleted' => "La suppression de l'attribution a échoué.",
+    ],
+    'auth' => [
+        'login' => "L'authentification a échoué.",
+        'logout' => "La déconnexion a échoué.",
+    ],
+    'description' => [
+        'created' => "Le genre n'a pas pu être lié à l'artiste.",
+        'deleted' => "Le genre n'a pas pu être retiré de l'artiste.",
+    ],
+    'equipment' => [
+        'created' => "La création de l'équipement a échoué.",
+        'deleted' => "La suppression de l'équipement a échoué.",
+        'restored' => "La restauration de l'équipement a échoué.",
+    ],
+    'event' => [
+        'created' => "La création de l'événement a échoué.",
+        'deleted' => "La suppression de l'événement a échoué.",
+        'updated' => "La mise à jour de l'événement a échoué.",
+        'unpublished' => "L'annulation de la publication de l'événement a échoué.",
+        'published' => "La publication de l'événement a échoué.",
+    ],
+    'event_type' => [
+        'created' => "La création du type d'événement a échoué.",
+        'deleted' => "La suppression du type d'événement a échoué.",
+        'restored' => "La restauration du type d'événement a échoué.",
+    ],
+    'facebook' => [
+        'no_action' => "Aucune action n'a été faite.",
+        'wrong_user' => "Le compte Facebook avec lequel vous êtes authentifié ne peut pas effectuer cette action.",
+    ],
+    'file' => [
+        'deleted' => "La suppression du fichier \":name\" a échoué.",
+        'uploaded' => "La sauvegarde du fichier \":name\" a échoué.",
+    ],
+    'fulfillment' => [
+        'created' => "L'assignation de la compétence a échoué.",
+        'deleted' => "La suppression de la compétence assignée a échoué.",
+    ],
+    'genre' => [
+        'created' => "La création du genre a échoué.",
+        'deleted' => "La suppression du genre a échoué.",
+        'restored' => "La restauration du genre a échoué.",
+    ],
+    'gift' => [
+        'created' => "La création du lot a échoué.",
+        'deleted' => "La suppression du lot a échoué.",
+        'restored' => "La restauration du lot a échoué.",
+    ],
+    'guarantee' => [
+        'created' => "Le représentant n'a pas pu être lié à l'événement.",
+        'deleted' => "Le représentant n'a pas pu être retiré de l'événement.",
+    ],
+    'illustration' => [
+        'created' => "L'image n'a pas pu être liée à l'artiste.",
+        'deleted' => "L'image n'a pas pu être retirée de l'artiste.",
+    ],
+    'image' => [
+        'updated' => "La mise à jour de l'image a échoué.",
+        'created' => "La création de l'image a échoué.",
+        'deleted' => "La suppression de l'image a échoué.",
+        'restored' => "La restauration de l'image a échoué.",
+    ],
+    'instrument' => [
+        'created' => "La création de l'instrument a échoué.",
+        'restored' => "La restauration de l'instrument a échoué.",
+        'deleted' => "La suppression de l'instrument a échoué.",
+    ],
+    'language' => [
+        'created' => "La création de le langue a échoué.",
+        'updated' => "La mis à jour de le langue a échoué.",
+        'deleted' => "La suppression de le langue a échoué.",
+        'restored' => "La restauration de le langue a échoué.",
+    ],
+    'lineup' => [
+        'created' => "L'enregistrement de la formation a échoué.",
+        'deleted' => "La suppression de la formation a échoué.",
+    ],
+    'link' => [
+        'created' => "La création du lien a échoué.",
+        'updated' => "La mis à jour du lien a échoué.",
+        'deleted' => "La suppression du lien a échoué.",
+        'restored' => "La restauration du lien a échoué.",
+    ],
+    'member' => [
+        'created' => "La création du membre a échoué.",
+        'updated' => "La mis à jour du membre a échoué.",
+        'deleted' => "La suppression du membre a échoué.",
+        'restored' => "La restauration du membre a échoué.",
+    ],
+    'musician' => [
+        'created' => "La création du musicien \":name\" a échoué.",
+        'deleted' => "La suppression du musicien \":name\" a échoué.",
+    ],
+    'need' => [
+        'created' => "La création du besoin a échoué.",
+        'deleted' => "La suppression du besoin a échoué.",
+        'updated' => "La mise à jour du besoin a échoué.",
+        'restored' => "La restauration du besoin a échoué.",
+    ],
+    'offer' => [
+        'created' => "La création de l'offre a échoué.",
+        'updated' => "La mise à jour de l'offre a échoué.",
+        'deleted' => "La suppression de l'offre a échoué.",
+    ],
+    'performer' => [
+        'created' => "L'enregistrement de la performance de l'artiste a échoué.",
+        'updated' => "La mise à jour de la performance de l'artiste a échoué.",
+        'deleted' => "La suppression de la performance de l'artiste a échoué.",
+    ],
+    'printing' => [
+        'created' => "La création de l'imprimé a échoué.",
+        'updated' => "La mise à jour de l'imprimé a échoué.",
+        'deleted' => "La suppression de l'imprimé a échoué.",
+    ],
+    'printing_type' => [
+        'created' => "La création du type d'imprimé a échoué.",
+        'deleted' => "La suppression du type d'imprimé a échoué.",
+        'restored' => "La restauration du type d'imprimé a échoué.",
+    ],
+    'representer' => [
+        'created' => "La création du représentant a échoué.",
+        'updated' => "La mise à jour du représentant a échoué.",
+        'deleted' => "La suppression du représentant a échoué.",
+        'restored' => "La restauration du représentant a échoué.",
+    ],
+    'sharing' => [
+        'created' => "Le partage de l'événement a échoué.",
+        'updated' => "La mise à jour du partage a échoué.",
+        'deleted' => "La suppression du partage a échoué.",
+        'no_platform' => "Le partage sur cette plate-forme n'est pas encore supportée par l'application.",
+    ],
+    'skill' => [
+        'created' => "La création de la compétence a échoué.",
+        'deleted' => "La suppression de la compétence a échoué.",
+        'restored' => "La restauration de la compétence a échoué.",
+    ],
+    'staff' => [
+        'created' => "L'assignation du rôle a échoué.",
+        'updated' => "La mise à jour du rôle a échoué.",
+        'deleted' => "La suppression du rôle a échoué.",
+    ],
+    'symbolization' => [
+        'created' => "L'image n'a pas pu être liée à l'événement.",
+        'deleted' => "L'image n'a pas pu être retirée de l'événement.",
+    ],
+    'ticket' => [
+        'created' => "La création du ticket d'entrée a échoué.",
+        'deleted' => "La suppression du ticket d'entrée a échoué.",
+        'updated' => "La mise à jour du ticket d'entrée a échoué.",
+    ],
+    'ticket_category' => [
+        'created' => "La création de la catégorie de ticket a échoué.",
+        'deleted' => "La suppression de la catégorie de ticket a échoué.",
+        'restored' => "La restauration de la catégorie de ticket a échoué.",
+    ],
+];
--- app/lang/en/fail.php
+++ app/lang/en/fail.php
@@ -4,20 +4,170 @@
  * Fail messages
  */
 
-return array(
-    'language' => array(
-        'inexistant' => "La langue n'existe pas",
-        'missing' => "L'attribut 'locale' est manquant",
-    ),
-    'routes' => array(
-        'missing' => "Oups! Cette url n'existe pas",
-    ),
-    'event' => array(
-        'overlap' => "Il y a déjà un événement planifié pour cette date là !",
-    ),
-    'representer' => array(
-        'inexistant' => "Le représentant demandé n'existe pas.",
-    ),
+return [
     'acl' => "Vous n'avez pas le droit d'effectuer cette action",
+    'artist' => [
+        'inexistant' => "Il n'existe aucun artiste correspondant.",
+        'nogenre' => "Aucun genre valide pour cet artiste.",
+        'genre' => "Le genre :key n'existe pas.",
+        'image' => "L'image :key n'existe pas ou n'est pas disponible.",
+    ],
+    'attribution' => [
+        'existing' => "Il y a déjà une attribution de même type enregistrée.",
+        'inexistant' => "Il n'existe aucune attribution correspondante.",
+        'not_unique' => "doublons !",
+    ],
     'auth' => "Vous devez être authentifié pour effectuer cette action",
-);
+    'description' => [
+        'existing' => "Cet artiste est déjà lié à ce genre.",
+        'inexistant' => "Il n'existe aucune description correspondante.",
+        'last_genre' => "Ce genre est le dernier pour cet artiste, vous ne pouvez pas le supprimer.",
+    ],
+    'empty_data' => "Il n'y a rien à mettre à jour.",
+    'equipment' => [
+        'existing' => "Il y a déjà un équipement \":name\" enregistré.",
+        'inexistant' => "Il n'existe aucun équipement correspondant.",
+    ],
+    'event' => [
+        'overlap' => "Cet événement chevauche un événement déjà existant.",
+        'existing' => "Il y a déjà un événement \":name\" enregistré.",
+        'inexistant' => "Il n'existe aucun événement correspondant.",
+        'at_least_one_main_performer' => "L'événement doit accueillir au moins un groupe principal.",
+        'is_symbolized' => "L'événement doit avoir une image de couverture.",
+        'is_not_published' => "L'événement doit être publié pour que un post peut être crée.",
+    ],
+    'event_type' => [
+        'existing' => "Il y a déjà un type d'événement \":name\" enregistré.",
+        'inexistant' => "Il n'existe aucun type d'événement correspondant.",
+    ],
+    'export' => [
+        'no_input' => "Des dates de/à n'ont pas été trouvés.",
+        'unchronological' => "Les dates données ne sont pas chronologiquement correcte.",
+    ],
+    'file' => [
+        'invalid' => "Le fichier sélectionné n'est pas un fichier valide.",
+        'inexistant' => "Le fichier \":file\" n'existe pas sur ce serveur.",
+    ],
+    'file_type' => [
+        'unsupported' => "Vous ne pouvez pas sauvegarder de fichier de ce type (:type).",
+    ],
+    'fulfillment' => [
+        'existing' => "Cette compétence est déjà assignée à ce membre.",
+        'inexistant' => "Il n'existe aucune compétence correspondante assignée à ce membre.",
+        'non_assigned' => "Cette compétence ne peut pas être remplie par ce membre.",
+    ],
+    'genre' => [
+        'existing' => "Il y a déjà un genre \":name\" enregistré.",
+        'inexistant' => "Il n'existe aucun genre correspondant.",
+    ],
+    'gift' => [
+        'existing' => "Il y a déjà un lot \":name\" enregistré.",
+        'inexistant' => "Il n'existe aucun lot correspondant.",
+    ],
+    'guarantee' => [
+        'existing' => "Les artistes de cet événement sont déjà représentés par un autre représentant.",
+        'inexistant' => "Il n'y a aucun représentant rattaché à cet événement.",
+    ],
+    'illustration' => [
+        'existing' => "Cette image illustre déjà un autre artiste.",
+        'inexistant' => "Cette image n'illustre aucun artiste.",
+        'inadequate' => "Cette image n'illustre pas cet artiste.",
+    ],
+    'image' => [
+        'inexistant' => "Il n'existe aucune image correspondants.",
+        'existing' => "Il y a déjà une image enregistrée pour \":name\".",
+    ],
+    'instrument' => [
+        'existing' => "Il y a déjà un instrument \":name\" enregistré.",
+        'inexistant' => "Il n'existe aucun instrument correspondant.",
+    ],
+    'language' => [
+        'inexistant' => "Il n'existe aucune langue correspondante.",
+        'missing' => "L'attribut \"locale\" est manquant",
+    ],
+    'lineup' => [
+        'existing' => "Ce musicien joue déjà de cet instrument pour cet artiste.",
+        'inexistant' => "Il n'existe aucune formation correspondante.",
+        'last_lineup' => "Cette formation est la dernière pour ce musicien, vous ne pouvez pas la supprimer.",
+    ],
+    'link' => [
+        'inexistant' => "Il n'existe aucun lien correspondant.",
+    ],
+    'member' => [
+        'inexistant' => "Il n'existe aucun membre correspondant.",
+        'existing' => "Il y a déjà un membre \":name\" enregistré.",
+    ],
+    'musician' => [
+        'existing' => "Il y a déjà un musician \":name\" enregistré.",
+        'inexistant' => "Il n'existe aucun musician correspondant.",
+        'nolineup' => "Aucune formation valide pour ce musicien.",
+        'no_instrument_artist' => "L'instrument et l'artiste n'existent pas pour la formation :key.",
+        'no_instrument' => "L'instrument n'existe pas pour la formation :key.",
+        'no_artist' => "L'artiste n'existe pas pour la formation :key.",
+    ],
+    'need' => [
+        'existing' => "Il y a déjà un besoin de même type enregistré.",
+        'inexistant' => "Il n'existe aucun besoin correspondant.",
+        'non_needed' => "Cette compétence n'est pas demandée lors de cet événement.",
+        'not_unique' => "Les besoins ont des doublons !",
+    ],
+    'offer' => [
+        'existing' => "Il y a déjà une offre de même type enregistrée.",
+        'inexistant' => "Il n'existe aucune offre correspondante.",
+        'not_unique' => "doublons !",
+    ],
+    'performer' => [
+        'existing' => "Ce groupe joue déjà dans cet événement à cette position.",
+        'inexistant' => "Il n'existe aucun performeur correspondant.",
+        'order_not_available' => "Cette position de passage est déjà occupée par un autre artiste.",
+        'not_unique' => "Les performeurs ont des doublons !",
+    ],
+    'printing' => [
+        'existing' => "Ce type d'imprimé est déjà lié à cet événement.",
+        'inexistant' => "Il n'existe aucun imprimé correspondant.",
+    ],
+    'printing_type' => [
+        'existing' => "Il y a déjà un type d'imprimé \":name\" enregistré.",
+        'inexistant' => "Il n'existe aucun type d'imprimé correspondant.",
+    ],
+    'representer' => [
+        'inexistant' => "Il n'existe aucun représenant correspondant.",
+        'existing' => "Il existe déjà un représentant \":name\" enregistré.",
+    ],
+    'routes' => [
+        'missing' => "Oups! Cette url n'existe pas",
+    ],
+    'sharing' => [
+        'inexistant' => "Il n'existe aucun sharing correspondant.",
+    ],
+    'skill' => [
+        'existing' => "Il y a déjà une compétence \":name\" enregistrée.",
+        'inexistant' => "Il n'existe aucune compétence correspondante.",
+    ],
+    'staff' => [
+        'existing' => "Ce membre effectue déjà un autre rôle pour cet événement.",
+        'inexistant' => "Il n'existe aucun staff correspondant.",
+        'not_unique' => "Un membre ne peut pas remplire plusieurs fonctions dans la meme soirée !",
+    ],
+    'symbolization' => [
+        'existing' => "Cet événement à déjà une image de couverture.",
+        'inexistant' => "Cet événement n'a pas d'image de couverture.",
+        'attach_image_not_performer' => "Cette image n'illustre aucun artiste jouant dans cet événement, il n'est pas possible de l'utiliser comme image de couverture",
+    ],
+    'ticket' => [
+        'existing' => "Il y a déjà un ticket d'entrée de même type enregistré.",
+        'inexistant' => "Il n'existe aucun ticket d'entrée correspondant.",
+        'last_ticket' => "Ce ticket d'entrée est le dernier pour cet événement, vous ne pouvez pas le supprimer.",
+    ],
+    'ticket_category' => [
+        'existing' => "Il y a déjà une catégorie de tickets \":name\" enregistrée.",
+        'inexistant' => "Il n'existe aucune catégorie de tickets correspondante.",
+        'not_unique' => "...doublon...",
+    ],
+    'wordexport' => [
+        'noinput' => "Les dates de début et de fin de la période à exporter sont obligatoires.",
+    ],
+    'xmlexport' => [
+        'noinput' => "Les dates de début et de fin de la période à exporter sont obligatoires.",
+    ],
+];
