dc1b94fa3747b89e3fb147b58168f27188b1a099
GuggerJoel <joel.gugger@rdlp.ch>
2014-07-03 19:31:53 +0200

    made in prod

--- app/start/global.php
+++ app/start/global.php
@@ -48,7 +48,7 @@ Log::useFiles(storage_path() . '/logs/laravel.log');
 
 App::error(function(Exception $exception, $code) {
     Log::error($exception);
-    //return Jsend::error(trans('error.app', ['code' => $code]));
+    return Jsend::error(trans('error.app', ['code' => $code]));
 });
 
 /*
