3d2f2773eb115b5be2535f1509df3d3c0d0edda0
Tazaf <obersonmathias@gmail.com>
2014-07-03 15:08:19 +0200

    updated DataBaseSeeder => Added a static property containing the namespace for the Seeds

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 class DatabaseSeeder extends Seeder {
+    
+    protected static $namespace = 'Rockit\\Database\\Seeders\\';
 
     /**
      * Run the database seeds.
@@ -10,69 +12,69 @@ class DatabaseSeeder extends Seeder {
     public function run() {
         Eloquent::unguard();
 
-        $this->call('Rockit\Database\Seeders\InstrumentsTableSeeder');
+        $this->call(self::$namespace . 'InstrumentsTableSeeder');
         $this->command->info('Instruments table seeded!');
-        $this->call('Rockit\Database\Seeders\GenresTableSeeder');
+        $this->call(self::$namespace . 'GenresTableSeeder');
         $this->command->info('Genres table seeded!');
-        $this->call('Rockit\Database\Seeders\MusiciansTableSeeder');
+        $this->call(self::$namespace . 'MusiciansTableSeeder');
         $this->command->info('Musucians table seeded !');
-        $this->call('Rockit\Database\Seeders\PlatformsTableSeeder');
+        $this->call(self::$namespace . 'PlatformsTableSeeder');
         $this->command->info('Platforms table seeded !');
-        $this->call('Rockit\Database\Seeders\TicketCategoriesTableSeeder');
+        $this->call(self::$namespace . 'TicketCategoriesTableSeeder');
         $this->command->info('Ticket Categories table seeded !');
-        $this->call('Rockit\Database\Seeders\EquipmentsTableSeeder');
+        $this->call(self::$namespace . 'EquipmentsTableSeeder');
         $this->command->info('Equipments table seeded !');
-        $this->call('Rockit\Database\Seeders\GiftsTableSeeder');
+        $this->call(self::$namespace . 'GiftsTableSeeder');
         $this->command->info('Gifts table seeded !');
-        $this->call('Rockit\Database\Seeders\EventTypesTableSeeder');
+        $this->call(self::$namespace . 'EventTypesTableSeeder');
         $this->command->info('Event Types table seeded !');
-        $this->call('Rockit\Database\Seeders\PrintingsTypesTableSeeder');
+        $this->call(self::$namespace . 'PrintingsTypesTableSeeder');
         $this->command->info('Printing Types table seeded !');
-        $this->call('Rockit\Database\Seeders\SkillsTableSeeder');
+        $this->call(self::$namespace . 'SkillsTableSeeder');
         $this->command->info('Skills table seeded !');
-        $this->call('Rockit\Database\Seeders\RepresentersTableSeeder');
+        $this->call(self::$namespace . 'RepresentersTableSeeder');
         $this->command->info('Representers table seeded !');
-        $this->call('Rockit\Database\Seeders\ArtistsTableSeeder');
+        $this->call(self::$namespace . 'ArtistsTableSeeder');
         $this->command->info('Artist table seeded !');
-        $this->call('Rockit\Database\Seeders\ImagesTableSeeder');
+        $this->call(self::$namespace . 'ImagesTableSeeder');
         $this->command->info('Images table seeded !');
-        $this->call('Rockit\Database\Seeders\EventsTableSeeder');
+        $this->call(self::$namespace . 'EventsTableSeeder');
         $this->command->info('Events table seeded !');
-        $this->call('Rockit\Database\Seeders\LinksTableSeeder');
+        $this->call(self::$namespace . 'LinksTableSeeder');
         $this->command->info('Links table seeded !');
-        $this->call('Rockit\Database\Seeders\MembersTableSeeder');
+        $this->call(self::$namespace . 'MembersTableSeeder');
         $this->command->info('Members table seeded !');
-        $this->call('Rockit\Database\Seeders\DescriptionTableSeeder');
+        $this->call(self::$namespace . 'DescriptionTableSeeder');
         $this->command->info('Description table seeded !');
-        $this->call('Rockit\Database\Seeders\SharingsTableSeeder');
+        $this->call(self::$namespace . 'SharingsTableSeeder');
         $this->command->info('Sharings table seeded !');
-        $this->call('Rockit\Database\Seeders\TicketsTableSeeder');
+        $this->call(self::$namespace . 'TicketsTableSeeder');
         $this->command->info('Tickets table seeded !');
-        $this->call('Rockit\Database\Seeders\AttributionsTableSeeder');
+        $this->call(self::$namespace . 'AttributionsTableSeeder');
         $this->command->info('Attribution table seeded !');
-        $this->call('Rockit\Database\Seeders\OffersTableSeeder');
+        $this->call(self::$namespace . 'OffersTableSeeder');
         $this->command->info('Offer table seeded !');
-        $this->call('Rockit\Database\Seeders\PrintingsTableSeeder');
+        $this->call(self::$namespace . 'PrintingsTableSeeder');
         $this->command->info('Printings table seeded !');
-        $this->call('Rockit\Database\Seeders\FulfillmentsTableSeeder');
+        $this->call(self::$namespace . 'FulfillmentsTableSeeder');
         $this->command->info('Fulfillments table seeded !');
-        $this->call('Rockit\Database\Seeders\NeedsTableSeeder');
+        $this->call(self::$namespace . 'NeedsTableSeeder');
         $this->command->info('Needs table seeded !');
-        $this->call('Rockit\Database\Seeders\StaffsTableSeeder');
+        $this->call(self::$namespace . 'StaffsTableSeeder');
         $this->command->info('table seeded !');
-        $this->call('Rockit\Database\Seeders\PerformersTableSeeder');
+        $this->call(self::$namespace . 'PerformersTableSeeder');
         $this->command->info('Performer table seeded !');
-        $this->call('Rockit\Database\Seeders\LineupsTableSeeder');
+        $this->call(self::$namespace . 'LineupsTableSeeder');
         $this->command->info('Lineup table seeded !');
-        $this->call('Rockit\Database\Seeders\ResourceTableSeeder');
+        $this->call(self::$namespace . 'ResourceTableSeeder');
         $this->command->info('Resource table seeded !');
-        $this->call('Rockit\Database\Seeders\GroupTableSeeder');
+        $this->call(self::$namespace . 'GroupTableSeeder');
         $this->command->info('Group table seeded !');
-        $this->call('Rockit\Database\Seeders\LanguageTableSeeder');
+        $this->call(self::$namespace . 'LanguageTableSeeder');
         $this->command->info('Language table seeded !');
-        $this->call('Rockit\Database\Seeders\UserTableSeeder');
+        $this->call(self::$namespace . 'UserTableSeeder');
         $this->command->info('User table seeded !');
-        $this->call('Rockit\Database\Seeders\GroupResourceTableSeeder');
+        $this->call(self::$namespace . 'GroupResourceTableSeeder');
         $this->command->info('Group_Resource table seeded !');
     }
 
